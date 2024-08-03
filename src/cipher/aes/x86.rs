e011f6eaf03e1d366d3aa238c09a471e7e444df8
Michael Paddon <michael@paddon.org>
2024-08-03 09:59:01 +0900

    Cleanup test code.

--- src/cipher/aes/x86.rs
+++ src/cipher/aes/x86.rs
@@ -365,27 +365,6 @@ define_aes!{Aes128, 16, 10, expand_key128}
 define_aes!{Aes192, 24, 12, expand_key192}
 define_aes!{Aes256, 32, 14, expand_key256}
 
-pub fn mwp128() {
-    let key: [u8; 16] = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c];
-    let plaintext = [0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d, 0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34];
-    let ciphertext = [0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a, 0xb, 0x32];
-
-    let aes = Aes128::new(&key).unwrap();
-    let mut output = [0u8; 16];
-    aes.encrypt(plaintext.as_slice(), output.as_mut_slice());
-    assert_eq!(output, ciphertext);
-    aes.decrypt(ciphertext.as_slice(), output.as_mut_slice());
-    assert_eq!(output, plaintext);
-
-    /*
-    let plaintext = [0u8; 65536];
-    let mut output = [0u8; 65536];
-    for _ in 0..(1000 * 1000) {
-        aes.encrypt(plaintext.as_slice(), output.as_mut_slice());
-    }
-    */
-}
-
 #[cfg(test)]
 mod test {
     use crate::test::acvp::block;
